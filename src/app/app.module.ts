0624616262ce7a35788c3b69ef4ad8f96af7ffd3
Geetha1233 <50858509+Geetha1233@users.noreply.github.com>
2023-05-17 16:39:42 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,RouterModule
   ],
   imports: [
     BrowserModule,
